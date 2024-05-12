7a598ac70f9109e7e61f4a8cbe47966b2f7c7875
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-05-13 03:06:53 +0530

    nne

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const ProjectCard = ({ src, title, description }: Props) => {
   return (
-    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
+    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] z-[999]">
       {/* <Image
         src={src}
         alt={title}
@@ -18,13 +18,13 @@ const ProjectCard = ({ src, title, description }: Props) => {
         className="w-full object-contain"
       /> */}
 
-      <div className="relative p-4 text-center">
+      <div className="relative p-4 text-center z-50">
         <h1 className="text-2xl font-semibold text-white ">{title}</h1>
         <p className="mt-2 text-gray-300">{description}</p>
       </div>
       <div className=" flex justify-center items-center my-2">
 
-        <a href="/" className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px] px-4"> Contact Us</a>
+        <a href="/" className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px] px-4 cursor-pointer"> Contact Us</a>
 
       </div>
     </div>
