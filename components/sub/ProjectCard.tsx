75b015678cf5d1b6ab6ace249f159412540d4bb5
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-05-13 14:10:06 +0530

    Update ProjectCard.tsx

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const ProjectCard = ({ src, title, description }: Props) => {
   return (
-    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] z-[999]">
+    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] z-[999] max-w-full  sm:max-w-[450px] mx-auto">
       {/* <Image
         src={src}
         alt={title}
