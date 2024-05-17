7131cd9ff012fe7e113197a9ae2405ad8e3803f0
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-05-17 21:01:01 +0530

    Update Projects.tsx

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -11,27 +11,7 @@ const Projects = () => {
        Our Solana Bots
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
-        <ProjectCard
-          src="/NextWebsite.png"
-          title="Random volume and MC generator"
-          description="our cutting-edge Random Volume and Market Cap Generator! This powerful tool is designed to simulate market conditions by using multiple wallets to generate random trading volumes and market capitalizations."
-        />
-        <ProjectCard
-          src="/CardImage.png"
-          title="Multi wallet volume generator"
-          description="Our multi wallet Volume Generator! This powerful tool is designed to simulate market conditions by using multiple wallets to generate random trading volume."
-        />
-        <ProjectCard
-          src="/SpaceWebsite.png"
-          title="Pump.fun booster volume"
-          description="Our Bot  generates random trading volume, ensuring an exhilarating and immersive experience for traders. Get ready to amplify your trading potential like never before!"
-        />
-      </div>
-
-
-
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10 mt-4">
-        <ProjectCard
+      <ProjectCard
           src="/NextWebsite.png"
           title="Jito bundle multi wallet bundle"
           description="Create token  + Add Liquidity: The program allows you to enter desired amount of base tokens(COIN) and quote tokens(SOL), upon which it will create the liquidity pool.
@@ -43,6 +23,32 @@ const Projects = () => {
           description="Create token  + Add Liquidity: The program allows you to enter desired amount of base tokens(COIN) and quote tokens(SOL), upon which it will create the liquidity pool.
           this uses JITO Bundles to add liquidity to your token and makes the first swap (first txn). you can also freeze tnx "
         />
+       
+       
+        <ProjectCard
+          src="/NextWebsite.png"
+          title="Random volume and MC generator"
+          description="our cutting-edge Random Volume and Market Cap Generator! This powerful tool is designed to simulate market conditions by using multiple wallets to generate random trading volumes and market capitalizations."
+        />
+     
+      </div>
+
+
+
+      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10 mt-4">
+      <ProjectCard
+          src="/CardImage.png"
+          title="Multi wallet volume generator"
+          description="Our multi wallet Volume Generator! This powerful tool is designed to simulate market conditions by using multiple wallets to generate random trading volume."
+        />
+        <ProjectCard
+          src="/SpaceWebsite.png"
+          title="Pump.fun booster volume"
+          description="Our Bot  generates random trading volume, ensuring an exhilarating and immersive experience for traders. Get ready to amplify your trading potential like never before!"
+        />
+       
+       
+        
         <ProjectCard
           src="/SpaceWebsite.png"
           title="AirDrop Bundler"
